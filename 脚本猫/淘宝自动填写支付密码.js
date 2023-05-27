b62848d4ce3c67380a5c2dd20c93cfcdf5c3d435
duoluodexiaoxiaoyuan <1203125214@qq.com>
2023-05-27 16:24:58 +0800

    feature: 淘宝代码

---
+++ 脚本猫/淘宝自动填写支付密码.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         淘宝自动填写支付密码
+// @namespace    https://bbs.tampermonkey.net.cn/
+// @version      0.1.0
+// @description  try to take over the world!
+// @author       You
+// @match        https://cashiersa128.alipay.com/*
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  setTimeout(() => {
+      // 输入支付密码
+      document.querySelector('#payPassword_rsainput').value='123456'
+      setTimeout(() => {
+          // 点击付款
+          // document.querySelector('#J_authSubmit').click()
+      }, 1000)
+  }, 3000)
+})();
---
+++ 脚本猫/淘宝自动报名交保证金.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         淘宝报名
+// @namespace    https://bbs.tampermonkey.net.cn/
+// @version      0.1.0
+// @description  try to take over the world!
+// @author       You
+// @match        https://item-paimai.taobao.com/pmp_item/*
+// @run-at document-end
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  // @run-at document-end 表示脚本应该在文档加载完成后运行，但在所有图片、样式表和子框架加载完成之前运行。这通常是在 DOMContentLoaded 事件触发后，但在 load 事件触发之前。
+  setTimeout(() => {
+      let baoMingSpan = document.querySelector("#J_ApplyBtn > span:nth-child(2)")
+      if (baoMingSpan) {
+          document.querySelectorAll('.pm-button')[0].click()
+          setTimeout(() => {
+              if (document.querySelector(".bid-popup")) {
+                  document.querySelector('.submit-btn').click()
+              }
+          }, 1000)
+      }
+  },3000)
+})();
