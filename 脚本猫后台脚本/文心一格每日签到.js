2e7f1e4320ae98135869895a97c7305ffa7d2460
duoluodexiaoxiaoyuan <1203125214@qq.com>
2023-08-25 09:43:53 +0800

    feature: 实时获取时间戳防止封号

--- 脚本猫后台脚本/文心一格每日签到.js
+++ 脚本猫后台脚本/文心一格每日签到.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         文心一格每日签到获取电量
 // @namespace    https://bbs.tampermonkey.net.cn/
-// @version      0.1.0
+// @version      0.1.1
 // @description  每日签到获取电量，但是电量有效期是一个月，所以不用的话就不用安装脚本了
 // @author       小垃圾kiki
 // @crontab      * * once * *
@@ -10,26 +10,29 @@
 // @grant        GM_notification
 // ==/UserScript==
 
+
+
 return new Promise((resolve, reject) => {
-  GM_xmlhttpRequest({
-    method: "GET",
-    url: `https://yige.baidu.com/api/t2p/points/task_complete?t=1692885529005&appname=pc&ptask_type=6`,
-    data: formobj,
-    onload: xhr => {
-        console.log(xhr)
-        let data = JSON.parse(xhr.response)
-        let { message } = data
-        if (message === 'success') {
-          GM_notification(`文心一格签到成功`)
-        } else if (message === 'already signed') {
-          GM_notification(`文心一格，您已经签过到了，请明天再来吧`)
-        } else {
-          GM_notification(`文心一格，签到失败，请联系开发者处理`)
-        }
-    },
-    onerror: xhr => {
-        GM_notification("接口请求失败")
-    }
+  //  实时获取时间戳，防止封号
+ const timestamp = new Date().getTime();
+ GM_xmlhttpRequest({
+   method: "GET",
+   url: `https://yige.baidu.com/api/t2p/points/task_complete?t=${timestamp}&appname=pc&ptask_type=6`,
+   onload: xhr => {
+       console.log(xhr)
+       let data = JSON.parse(xhr.response)
+       let { message } = data
+       if (message === 'success') {
+         GM_notification(`文心一格签到成功`)
+       } else if (message === 'already signed') {
+         GM_notification(`文心一格，您已经签过到了，请明天再来吧`)
+       } else {
+         GM_notification(`文心一格，签到失败，请联系开发者处理`)
+       }
+   },
+   onerror: xhr => {
+       GM_notification("接口请求失败")
+   }
 })
 }).catch(err => {
 GM_notification("接口请求失败")
